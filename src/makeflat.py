0a9dfbd7336ff75ea1b09b4b569ff2e7d1472c7d
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2021-08-17 14:54:10 +0900

    Use arbitray number of flat frames for master flat

--- src/makeflat.py
+++ src/makeflat.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 """ Make a master flat frame from a 3-d fits
 2 types of normalization, median and mean, are implemented.
+
+Various flat frames (i.e. field of view, the number of frames) are acceptible.
+But all flat frames should have the same exposure time
+since unique dark frame is permitted as an input argument.
+
+The header info. of the 1st flat frame(s) is inherited.
 """
 from argparse import ArgumentParser as ap
 import os
@@ -9,23 +15,58 @@ import numpy as np
 import astropy.io.fits as fits
 
 
-def chopped_mean(cube):
-  """ Calculate chopped mean along axis-zero
+def norm2dflat(flat, dark, norm):
+  """Return a normalized flat frame from a flat frame.
 
-  Parameter
-  ---------
-    cube : numpy.ndarray
-      three-dimensional ndarray
+  Parameters
+  ----------
+  flat : np.ndarray
+    a 2-d flat frame
+  dark : np.ndarray
+    a 2-d dark frame
+  norm : str
+    normalization method (mean or median)
 
   Return
   ------
-    img : numpy.ndarray
-      stacked two-dimensional image
+  img : np.ndarray
+    a normalized 2-d flat frame
   """
-  nz, ny, nx = cube.shape
-  img = np.sum(cube, axis=0)-np.max(cube, axis=0)
-  img = img/(nz-1.0)
+  img = flat - dark
+  if norm=="mean":
+    img = img/np.mean(img)
+  elif norm=="median":
+    img = img/np.median(img)
   return img
+
+
+def norm3dflat(flat, dark, norm):
+  """Return a normalized flat frame from flat frames.
+
+  Parameters
+  ----------
+  flat : np.ndarray
+    a 3-d flat frames
+  dark : np.ndarray
+    a 2-d dark frame
+  norm : str
+    normalization method (mean or median)
+
+  Return
+  ------
+  img : np.ndarray
+    a normalized 2-d flat frame
+  """
+  nz, ny, nx = flat.shape
+  cube = []
+  for z in range(nz):
+    img = flat[z] - dark
+    if norm=="mean":
+      img = img/np.mean(img)
+    elif norm=="median":
+      img = img/np.median(img)
+    cube.append(img)
+  return np.median(cube, axis=0)
 
 
 def main(args):
@@ -36,39 +77,54 @@ def main(args):
   args : argparse.Namespace
     Arguments passed from the command-line as defined below.
   """
-  hdu_flat = fits.open(args.flat)
-  src_flat = hdu_flat[0]
-  flat = src_flat.data
-  hdr_flat = src_flat.header
-  assert len(flat.shape)==3, "Flat should be 3-d fits."
-
+  # Read a master dark frame
   hdu_dark = fits.open(args.dark)
   src_dark = hdu_dark[0]
   dark = src_dark.data
-  assert len(dark.shape)==2, "Dark should be 2-d fits."
+  assert len(dark.shape)==2, "A master dark frame should be 2-d fits."
+  print(f"  Read a dark frame {args.dark}")
   
-  img = chopped_mean(flat) - dark
+  # Read flat frames
+  N_flat = len(args.flat)
+  flatcube = []
+  for idx,fl in enumerate(args.flat):
+    hdu_flat = fits.open(fl)
+    src_flat = hdu_flat[0]
+    flat = src_flat.data
+    if idx==0:
+      hdr_flat = src_flat.header
+      hdr_fits = os.path.basename(fl)
+    
+    # 2d flat frame
+    if len(flat.shape)==2:
+      img = norm2dflat(flat, dark, args.norm)
+      # Add temporally cube 
+      flatcube.append(img)
+      
+    # 3d flat frames
+    elif len(flat.shape)==3:
+      img = norm3dflat(flat, dark, args.norm)
+      # Add temporally cube 
+      flatcube.append(img)
+    print(f"  Read a flat frame {fl}")
 
-  if args.norm=="mean":
-    # Normalize by mean
-    img = img/np.mean(img)
-  elif args.norm=="median":
-    # Normalize by median
-    # This option is useful as a first step 
-    #   for twilight flat frames including stars
-    img = img/np.median(img)
-
+  # Create a master flat from a flat cube
+  print(f"  Dimention of flat cube :{len(flatcube)}")
+  masterflat = np.median(flatcube, axis=0)
+  print(f"  Shape of a master flat :{masterflat.shape}")
   # Create 2-d master flat
-  flat = fits.PrimaryHDU(data=img, header=hdr_flat)
+  flat = fits.PrimaryHDU(data=masterflat, header=hdr_flat)
   # Add history
   hdr = flat.header
   hdr.add_history(
-    f"[makeflat] created from {os.path.basename(args.flat)}")
+    f"[makeflat] header info. is inherited from {hdr_fits}")
   hdr.add_history(
-    f"[makeflat] created from {os.path.basename(args.dark)}")
+    f"[makeflat] dark frame : {os.path.basename(args.dark)}")
+  for idx,fl in enumerate(args.flat):
+    hdr.add_history(
+      f"[makeflat] flat frame {idx+1} : {os.path.basename(fl)}")
   hdr.add_history(
-    f"[makeflat] normalization ({args.norm})")
-
+    f"[makeflat] normalization : {args.norm}")
 
   if args.out is None:
     out = f"f{os.path.basename(args.flat)}"
@@ -80,13 +136,13 @@ def main(args):
 
 
 if __name__ == "__main__":
-  parser = ap(description="Make a mater flat frame from a 3-d fits")
+  parser = ap(description="Make a mater flat frame from flat frame(s)")
   parser.add_argument(
-    "--flat", type=str, required=True,
+    "--flat", type=str, nargs="*", required=True,
     help="a raw flat frame")
   parser.add_argument(
     "--dark", type=str, required=True,
-    help="a raw dark frame")
+    help="a master dark frame")
   parser.add_argument(
     "--norm", type=str, default="mean",
     help="type of normalization (mean or median)")
