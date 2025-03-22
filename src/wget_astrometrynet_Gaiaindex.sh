1fa79880fe0a4109482a885dbdf9600eb2d294b4
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2025-03-22 16:15:04 +0100

    Get high-reso files

--- src/wget_astrometrynet_Gaiaindex.sh
+++ src/wget_astrometrynet_Gaiaindex.sh
@@ -1,9 +1,22 @@
 #! /bin/bash
 # Obtain index file for astrometry
 # See http://astrometry.net/doc/readme.html#getting-index-files and http://data.astrometry.net/ for details.
-# This index files work well for Seimei/TriCCS data (FoV ~ 12.6 arcmic x 7.5 arcmin)
+# index5203-xx files work well for Seimei/TriCCS data (FoV ~ 12.6 arcmic x 7.5 arcmin)
+
 # index-5205-*
+#for ((i=0; i<48; i++)); do
+#    I=$(printf %02i $i)
+#    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5205-$I.fits
+#done
+
+# index-5204-*
+#for ((i=0; i<48; i++)); do
+#    I=$(printf %02i $i)
+#    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5204-$I.fits
+#done
+
+# index-5203-*
 for ((i=0; i<48; i++)); do
     I=$(printf %02i $i)
-    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5205-$I.fits
+    wget https://portal.nersc.gov/project/cosmo/temp/dstn/index-5200/LITE/index-5203-$I.fits
 done
